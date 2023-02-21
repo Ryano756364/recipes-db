8fdcaa3dc67d33ab4201622b65c92e125a0d3425
rodell <ryanodell2668@gmail.com>
2023-02-21 17:22:52 -0500

    Setting up connection to database from Java

--- src/com/ryanpodell/Main.java
+++ src/com/ryanpodell/Main.java
@@ -1,7 +1,16 @@
 package com.ryanpodell;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\rpode\\Documents\\Coding\\recipes-db\\recipedb.db");
+        } catch (SQLException e){
+            System.out.println("Error -> " + e);
+        }
     }
 }
