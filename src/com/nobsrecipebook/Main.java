3f7d14bd0f68ef64d58748ec5aaea9d8ae414829
rodell <ryanodell2668@gmail.com>
2023-02-23 11:41:07 -0500

    Boilerplate of database connection setup

--- src/com/nobsrecipebook/Main.java
+++ src/com/nobsrecipebook/Main.java
@@ -1,7 +1,19 @@
 package com.nobsrecipebook;
 
+import com.ryanpodell.model.Datasource;
+
 public class Main {
     public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if (!datasource.open()) {
+            System.out.println("\nCannot open datasource");
+            return;
+        } else {
+            System.out.println("\nDatasource open!");
+        }
 
+
+        // Close connection
+        datasource.close();
     }
 }
--- src/com/nobsrecipebook/model/Datasource.java
+++ src/com/nobsrecipebook/model/Datasource.java
@@ -1,11 +1,43 @@
 package com.nobsrecipebook.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Datasource {
+
+    //Fields
+
     //public static final String DB_NAME = "recipedatacomplete.db";
-    public static final String DB_NAME = "testdata.db";
+    public static final String DB_NAME = "jsonPracticeDb.db";
 
     public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\rpode\\Documents\\" +
             "Coding\\recipes-db\\src\\com\\nobsrecipebook\\database" + DB_NAME;
+
+
+    //Connection control
+    private Connection conn;
+    public boolean open() {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error -> " + e);
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void close() {  //Added close for extra precaution
+        try {
+            if (conn != null) {
+                conn.close();
+                System.out.println("\nConnection successfully closed!");
+            }
+        } catch (SQLException e) {
+            System.out.println("\nCouldn't close connection -> " + e);
+            e.printStackTrace();
+        }
+    }
 
 
 }
